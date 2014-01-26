66f725ddb9e90670479c8b0c285dfb46eb10bb75
Kirk Elliott <kirk@dmvjs.com>
2014-01-26 13:44:47 -0500

    allows for functions and JS statements to be passed via JS, enabled by default for now

--- js/tagr/walkr.js
+++ js/tagr/walkr.js
@@ -9,14 +9,23 @@ define(["jquery", "tagr/tagr"], function($, tagr) {
       for (key in element) {
         if (element.hasOwnProperty(key)) {
           if (key !== "__contains__" && key !== "__element__") {
-            options[key] = element[key]
+            if (element[key]["__function__"]) {
+              var f = element[key]["__function__"];
+              if (f.substr(0, 8) === "function") {
+                options[key] = new Function('return ' + f)();
+              } else {
+                options[key] = new Function('return ' + f);
+              }
+            } else {
+              options[key] = element[key]
+            }
           }
         }
       }
 
       fragment = tagr(el, options);
 
-      if (element.__contains__ !== undefined && element.__contains__.length > 0) {
+      if (element.__contains__ !== undefined && element.__contains__.length) {
         $(element.__contains__).each(function (ind, ele) {
           ele.__parent__ = fragment;
         }).each(walkr);
