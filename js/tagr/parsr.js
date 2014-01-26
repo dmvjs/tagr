7b31d73968d9a621a1b7745a491eae9cd1f49aa2
Kirk Elliott <kirk@dmvjs.com>
2014-01-25 21:58:17 -0500

    adding dunders to private properties

--- js/tagr/parsr.js
+++ js/tagr/parsr.js
@@ -1,14 +1,14 @@
 define(["jquery", "tagr/tagr"], function($, tagr) {
     return function parsr(index, element) {
-      var el = element.element,
-        parent = element.parent || $(document.body),
+      var el = element.__element__,
+        parent = element.__parent__ || $(document.body),
         options = {},
         key,
         fragment;
 
       for (key in element) {
         if (element.hasOwnProperty(key)) {
-          if (key !== "contains" && key !== "element") {
+          if (key !== "__contains__" && key !== "__element__") {
             options[key] = element[key]
           }
         }
@@ -16,9 +16,9 @@ define(["jquery", "tagr/tagr"], function($, tagr) {
 
       fragment = tagr(el, options);
 
-      if (element.contains !== undefined && element.contains.length > 0) {
-        $(element.contains).each(function (ind, ele) {
-          ele.parent = fragment;
+      if (element.__contains__ !== undefined && element.__contains__.length > 0) {
+        $(element.__contains__).each(function (ind, ele) {
+          ele.__parent__ = fragment;
         }).each(parsr);
       }
 
