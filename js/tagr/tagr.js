2c8530bb8369e1f0f6b94559b6008725b0d69648
Kirk Elliott <kirk@dmvjs.com>
2014-01-26 16:37:29 -0500

    div is the default element type if no element is provided

--- js/tagr/tagr.js
+++ js/tagr/tagr.js
@@ -1,6 +1,7 @@
 define(["jquery"], function($) {
     var empty = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
-    return function (el, options) {
+    return function (element, options) {
+      var el = element || 'div';
       return $('<' + el + (($.inArray(el, empty) === -1) ? ' ' : '') + '/>', options);
     }
   }
