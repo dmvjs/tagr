f28efedceac8c31d867b780d827d012a3e5ad8ac
Kirk Elliott <kirk@dmvjs.com>
2014-01-26 16:39:37 -0500

    using ["__this__"] kind of private reference instead of dot notation implements the allowJsonFunctions to disable this by default

--- js/tagr/walkr.js
+++ js/tagr/walkr.js
@@ -1,8 +1,10 @@
-define(["jquery", "tagr/tagr"], function($, tagr) {
+define(["jquery", "tagr/tagr", "tagr/config"], function($, tagr, config) {
+    var $body = $(document.body),
+      allow = config.allowJsonFunctions || false;
     return function walkr(index, element) {
-      var el = element.__element__,
+      var el = element["__element__"],
         exclude = ["__contains__", "__element__", "__parent__"],
-        parent = element.__parent__ || $(document.body),
+        parent = element["__parent__"] || $body,
         options = {},
         key,
         fragment;
@@ -11,9 +13,11 @@ define(["jquery", "tagr/tagr"], function($, tagr) {
         if (element.hasOwnProperty(key)) {
           if ($.inArray(key, exclude) === -1) {
             if (element[key]["__function__"]) {
-              var f = element[key]["__function__"];
-              options[key] = (f.substr(0, 8) === "function") ? (new Function('return ' + f)()) :
-                (new Function('return ' + f));
+              if (allow) {
+                var f = element[key]["__function__"];
+                options[key] = (f.substr(0, 8) === "function") ? (new Function('return ' + f)()) :
+                  (new Function('return ' + f));
+              }
             } else {
               options[key] = element[key]
             }
@@ -23,9 +27,9 @@ define(["jquery", "tagr/tagr"], function($, tagr) {
 
       fragment = tagr(el, options);
 
-      if (element.__contains__ !== undefined && element.__contains__.length) {
-        $(element.__contains__).each(function (ind, ele) {
-          ele.__parent__ = fragment;
+      if (element["__contains__"] !== undefined && element["__contains__"].length) {
+        $(element["__contains__"]).each(function (ind, ele) {
+          ele["__parent__"] = fragment;
         }).each(walkr);
       }
 
